f4299f150edbbed4aeabc73cd2e486e123721253
preeraj <preeraj@umich.edu>
2021-02-24 01:06:01 +0530

    done

--- checkpoint1/checkpoint1b.py
+++ checkpoint1/checkpoint1b.py
@@ -102,7 +102,7 @@ def main():
     # now that the data is all clean, save your output to the `data` folder as 'starbucks_clean.csv'
     # you will use this file in checkpoint 2
     
-    df.to_csv(r'../data/starbucks_clean.csv')
+    df.to_csv(r'../data/starbucks_clean.csv',index = False)
     
 if __name__ == "__main__":
     main()
